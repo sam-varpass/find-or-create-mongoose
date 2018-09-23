ffff423e0ab7c615fee4720d797969f758b253c4
Sam Smith <sam@varpass.com>
2018-09-23 21:13:37 +0100

    Rebuild

--- test/support/database.js
+++ test/support/database.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB);
 
+before(done => {
+	console.log('Database connecting...');
+	mongoose.connection.on('connected', async () => {
+		console.log('Database connected');
+		done();
+	});
+});
+
 after(async () => {
 	await mongoose.connection.close();
 	console.log('Connection closed');
